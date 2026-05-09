19473f7055811bc454fb23908a9cc8a60422e9a5
1shaders <icloud.essence@icloud.com>
2026-05-10 05:48:44 +0800

    Update SwordState.lua

--- SwordState.lua
+++ SwordState.lua
@@ -1,22 +1,25 @@
-local v0, v1 = getgenv().spoof, getgenv().swordtype
+local v0 = getgenv().spoof
+local v1 = getgenv().swordtype
 local v3 = cloneref(game:GetService("RunService"))
+
 local v4 = workspace:WaitForChild(v0)
 local v2 = v4:WaitForChild("HandInvItem")
+local v5 = game:GetService("ReplicatedStorage"):FindFirstChild(v1, true)
 
-print("user --> " .. tostring(v4))
+print("user --> " .. v0)
 print("--> " .. tostring(v2))
-print("--> " .. tostring(v1 or "stone_sword"))
+print("--> " .. tostring(v4))
 
-if v2 then
+if v2 and v5 then
     for _, v in next, getconnections(v2.Changed) do 
         v:Disable() 
     end
 
     v3.RenderStepped:Connect(function()
         if v2 and v2.Parent then
-            v2.Value = v1 or "stone_sword"
+            v2.Value = v5
         end
     end)
 
-    v2.Value = v1 or "stone_sword"
+    v2.Value = v5
 end
